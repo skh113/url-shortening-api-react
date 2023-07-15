8dca2176b8f2320544c6c83488fc85279cc4ea9a
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-15 16:49:15 +0330

    Added Statics

--- src/components/StatisticItem.tsx
+++ src/components/StatisticItem.tsx
@@ -1,5 +1,25 @@
-const StatisticItem = () => {
-  return <div className="flex mt-8">item</div>;
+interface Props {
+  title: string;
+  imageSrc: string;
+  description: string;
+}
+
+const StatisticItem = ({ title, imageSrc, description }: Props) => {
+  return (
+    <div className="flex flex-col w-5/6 items-center bg-white rounded-md shadow-sm mx-6 my-8">
+      <div className="absolute h-full">
+        <div className="relative flex bg-my-dark-violet rounded-full justify-center items-center h-20 w-20 top-[-5%]">
+          <img
+            src={imageSrc}
+            alt="Static Logo"
+            className=" max-w-full max-h-full p-2"
+          />
+        </div>
+      </div>
+      <h4 className="font-bold text-3xl text-my-dark-violet mt-16">{title}</h4>
+      <p className="my-6 mx-12 text-my-grayish-violet">{description}</p>
+    </div>
+  );
 };
 
 export default StatisticItem;
--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -6,15 +6,38 @@ const Statistics = () => {
       <h2 className="font-bold text-4xl text-my-dark-violet">
         Advanced Statistics
       </h2>
-      <p className="text-my-grayish-violet mb-4">
+      <p className="text-my-grayish-violet my-4">
         Track how your links are performing across the web with our advanced
         statistics dashboard.
       </p>
 
-      <div>
-        <StatisticItem />
-        <StatisticItem />
-        <StatisticItem />
+      <div className="flex mobile:flex-col items-center lg:flex-row mx-10">
+        <StatisticItem
+          title="Brand Recognition"
+          description="Boost your brand recognition with
+            each click. Generic links don't
+            mean a thing. Branded links help
+            instil confidence in your content."
+          imageSrc="/images/icon-brand-recognition.svg"
+        />
+
+        <StatisticItem
+          title="Detailed Records"
+          description="Gain insights into who is clicking your
+            links. Knowing when and where
+            people engage with your content
+            helps inform better decisions."
+          imageSrc="/images/icon-detailed-records.svg"
+        />
+
+        <StatisticItem
+          title="Fully Customizable"
+          description="Improve brand awareness and
+          content discoverability through
+          customizable links. supercharging
+          audience engagement."
+          imageSrc="/images/icon-fully-customizable.svg"
+        />
       </div>
     </div>
   );
