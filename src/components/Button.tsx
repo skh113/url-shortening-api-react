e66c900668e59fcce365208ef30ea47b1a76ff2e
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-15 18:53:42 +0330

    Added desktop header

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,12 @@ import { twMerge } from "tailwind-merge";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button = ({ children, ...props }: Props) => {
+const Button = ({ children, className, ...props }: Props) => {
   return (
     <button
       className={twMerge(
-        `bg-my-cyan text-white px-10 py-3 rounded-3xl font-bold text-sm cursor-pointer`
+        `bg-my-cyan text-white px-10 py-3 rounded-3xl font-bold text-sm cursor-pointer`,
+        className
       )}
       {...props}
     >
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,32 @@
+import Button from "./Button";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex flex-row mx-16 my-6 items-center">
+      <a href="#" className="mx-5">
+        <img src="images/logo.svg" alt="Logo" />
+      </a>
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="mx-4">
+          <Button className="text-my-grayish-violet bg-white px-0 mx-4">
+            Features
+          </Button>
+          <Button className="text-my-grayish-violet bg-white px-0 mx-4">
+            Resources
+          </Button>
+          <Button className="text-my-grayish-violet bg-white px-0 mx-4">
+            Company
+          </Button>
+        </div>
+        <div className="">
+          <Button className="text-my-grayish-violet bg-white px-0 mx-4">
+            Login
+          </Button>
+          <Button>Sign Up</Button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
