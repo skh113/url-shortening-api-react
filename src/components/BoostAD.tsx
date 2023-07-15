39adaa3dcd781e5e94a5445d0451b60f01b26e75
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-15 16:32:59 +0330

    Fixed typography style

--- src/components/BoostAD.tsx
+++ src/components/BoostAD.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const BoostAD = ({ title }: Props) => {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-40 mobile:bg-boost-mobile md:bg-boost-desktop bg-center bg-cover bg-my-dark-violet">
+    <div className="flex flex-col justify-center items-center w-full mobile:bg-boost-mobile md:bg-boost-desktop bg-center bg-cover bg-my-dark-violet text-center py-12">
       <h2 className="text-white font-bold text-4xl mb-6">{title}</h2>
       <Button>Get Started</Button>
     </div>
