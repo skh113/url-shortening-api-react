43e376690e0c50c7d13c836b0cb4927d3a8ef29e
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-15 18:37:50 +0330

    Added Social media links

--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -1,26 +1,22 @@
 const SocialMediaIcons = () => {
   return (
     <div className="flex flex-row items-center w-full justify-evenly">
-      <img
-        src="/images/icon-facebook.svg"
-        alt="instagram"
-        className="cursor-pointer"
-      />
-      <img
-        src="/images/icon-twitter.svg"
-        alt="instagram"
-        className="cursor-pointer"
-      />
-      <img
-        src="/images/icon-pinterest.svg"
-        alt="instagram"
-        className="cursor-pointer"
-      />
-      <img
-        src="/images/icon-instagram.svg"
-        alt="instagram"
-        className="cursor-pointer"
-      />
+      <a href="https://github.com/skh113" target="_blank">
+        <img src="/images/github.svg" alt="github" />
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/real-keyvan-hosseini/"
+        target="_blank"
+      >
+        <img src="/images/linkedin-in.svg" alt="linkedin" />
+      </a>
+      <a href="https://t.me/Da_Real_K1" target="_blank">
+        <img src="/images/telegram.svg" alt="telegram" />
+      </a>
+      <a href="https://instagram.com/keyvan_hosseini1" target="_blank">
+        <img src="/images/instagram.svg" alt="instagram" />
+      </a>
     </div>
   );
 };
