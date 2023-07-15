0a899646ace38bb2d7e81fa064065c0756b8af6e
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-15 18:06:38 +0330

    Refactored Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,20 @@
-interface Props {
-  children: React.ReactNode;
-}
+import { twMerge } from "tailwind-merge";
 
-const Button = ({ children }: Props) => {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = ({ children, ...props }: Props) => {
   return (
-    <button className="bg-my-cyan text-white px-10 py-3 rounded-3xl font-bold text-sm cursor-pointer">
+    <button
+      className={twMerge(
+        `bg-my-cyan text-white px-10 py-3 rounded-3xl font-bold text-sm cursor-pointer`
+      )}
+      {...props}
+    >
       {children}
     </button>
   );
 };
+
+Button.displayName = "Button";
 
 export default Button;
