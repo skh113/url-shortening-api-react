bdbac181ec3427c4f55c8cafe3e8c5ab2c40eef4
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-16 18:37:46 +0330

    Added burger menu skeleton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,44 @@
 import Button from "./Button";
 
+import { useState } from "react";
+
 const Header = () => {
+  const [burgerMenu, setBurgerMenu] = useState(false);
+
   return (
-    <header className="flex flex-row mx-16 my-6 items-center">
+    <nav className="flex flex-row mx-16 my-6 items-center mobile:justify-between">
       <a href="#" className="mx-5">
         <img src="images/logo.svg" alt="Logo" />
       </a>
-      <div className="flex flex-row items-center justify-between w-full">
-        <div className="mx-4">
-          <Button className="text-my-grayish-violet bg-white px-0 mx-4">
-            Features
-          </Button>
-          <Button className="text-my-grayish-violet bg-white px-0 mx-4">
-            Resources
-          </Button>
-          <Button className="text-my-grayish-violet bg-white px-0 mx-4">
-            Company
-          </Button>
-        </div>
+      <div className="flex lg:hidden cursor-pointer">
+        <img
+          src="/images/bars.svg"
+          alt="Menu"
+          onClick={() => setBurgerMenu(!burgerMenu)}
+        />
+      </div>
+      <div className="flex flex-row items-center justify-between w-full mobile:hidden lg:flex">
         <div className="">
-          <Button className="text-my-grayish-violet bg-white px-0 mx-4">
-            Login
-          </Button>
-          <Button>Sign Up</Button>
+          <div className="mx-4">
+            <Button className="text-my-grayish-violet bg-white px-0 mx-4">
+              Features
+            </Button>
+            <Button className="text-my-grayish-violet bg-white px-0 mx-4">
+              Resources
+            </Button>
+            <Button className="text-my-grayish-violet bg-white px-0 mx-4">
+              Company
+            </Button>
+          </div>
+          <div className="">
+            <Button className="text-my-grayish-violet bg-white px-0 mx-4">
+              Login
+            </Button>
+            <Button>Sign Up</Button>
+          </div>
         </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
